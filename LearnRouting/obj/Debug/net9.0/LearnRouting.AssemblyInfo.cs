1c0ca72c055e42f1c3265f1374f3590617c19955
Phounn <bsanthasith@gmail.com>
2025-01-13 21:21:39 +0700

    PC

--- LearnRouting/obj/Debug/net9.0/LearnRouting.AssemblyInfo.cs
+++ LearnRouting/obj/Debug/net9.0/LearnRouting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnRouting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d129ba302740d4dc1853c35b7048a7b2cfb0625")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnRouting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnRouting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d129ba302740d4dc1853c35b7048a7b2cfb0625")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
